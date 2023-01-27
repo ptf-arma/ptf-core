7f614430a260c5851c6ab34c1f736ffbf9d1923c
BlakeSoarin <blakeherinK@gmail.com>
2023-01-27 16:35:26 -0500

    Changed config for Super Tucano and added PTF textures

---
+++ addons/PTF_Aircraft/cfgSuperTucano.hpp
@@ -0,0 +1,23 @@
+class PTF_SuperTucano : RHSGREF_A29B_HIDF
+{
+   scope = 2;
+   editorCategory = "PTF";
+   editorSubcategory = "PTF_Air";
+   ace_cargo_space = 0;
+   ace_cargo_hasCargo = 0;
+   ace_cargo_size = 0;
+   ace_cargo_canLoad = 0;
+   ace_dragging_canCarry = 0;
+   ace_dragging_canDrag = 0;
+   maximumLoad = 375;
+   driverCanSee = 31;
+   gunnerCanSee = 31;
+   faction = "BLU_F";
+   side = 1;
+   crew = "B_Pilot_F";
+   fuelConsumptionRate = 0.1;
+   fuelCapacity = 500;
+   author = "Paramarine Task Force";
+   displayName = "SuperTucano [MAG36]";
+   hiddenselectionstextures[] = {"PTF_Textures\aircraft\Super Tucano\Trainer\ST_Fuselage_Navy_co.paa","PTF_Textures\aircraft\Super Tucano\Trainer\ST_WingsAndStabs_Navy_co.paa","PTF_Textures\aircraft\Super Tucano\Trainer\ST_EverythingElse_Navy_co.paa",""};		
+};
