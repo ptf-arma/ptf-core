fc44a5adee7a8876ea2d866a26d12e731320964d
Lost <34311095+FireStone1029@users.noreply.github.com>
2023-04-15 16:26:27 -0400

    the big clean up
    did all the mag tickets as well as adding training rounds & ladder to arsenal

--- addons/PTF_Aircraft/cfgC17.hpp
+++ addons/PTF_Aircraft/cfgC17.hpp
@@ -3,63 +3,13 @@ class PTF_C17 : USAF_C17
    scope = 2;
    editorCategory = "PTF";
    editorSubcategory = "PTF_Air";
-   ace_cargo_space = 0;
    ace_cargo_hasCargo = 0;
-   ace_cargo_size = 0;
-   ace_cargo_canLoad = 0;
-   ace_dragging_canCarry = 0;
-   ace_dragging_canDrag = 0;
    maximumLoad = 375;
-   driverCanSee = 31;
-   gunnerCanSee = 31;
    faction = "BLU_F";
    side = 1;
-   crew = "B_Pilot_F";
-   fuelConsumptionRate = 0.1;
-   fuelCapacity = 1000;
    author = "Paramarine Task Force";
    displayName = "C-17 [MAG36]";
-   cargoCompartments[] = {"Compartment2"};
-
    hiddenSelections[] = {"c17-1", "c17-2", "c17-3", "c17-5", "c17-6", "c17-7_t", "c17-8", "eng_t", "lwng", "rwng", "tail", "hourformat1", "hourformat2", "minformat1", "minformat2", "secformat1", "secformat2", "n_gear", "l_gear_1", "l_gear_2", "r_gear_1", "r_gear_2"};
    hiddenSelectionsTextures[] = {"\PTF_Textures\Aircraft\C17\C17-1_co.paa", "\PTF_Textures\Aircraft\C17\C17-2_co.paa", "\PTF_Textures\Aircraft\C17\C17-3_co.paa", "\PTF_Textures\Aircraft\C17\C17-5_co.paa", "\PTF_Textures\Aircraft\C17\C17-6_co.paa", "\PTF_Textures\Aircraft\C17\C17-7_t_co.paa", "\PTF_Textures\Aircraft\C17\C17-8_co.paa", "\PTF_Textures\Aircraft\C17\C17-eng_co.paa", "\PTF_Textures\Aircraft\C17\Lwng_co.paa", "\PTF_Textures\Aircraft\C17\Rwng_co.paa", "\PTF_Textures\Aircraft\C17\Tail_co.paa"};
 
-   class TextureSources
-   {
-   };
-
-   class VehicleTransport
-   {
-      class Carrier
-      {
-         cargoBayDimensions[] = {"VIV_point_1", "VIV_point_2"};
-         disableHeightLimit = 1;
-         maxLoadMass = 137250;
-         cargoAlignment[] = {"front", "center"};
-         cargoSpacing[] = {0, 0, 0};
-         exits[] = {"VIV_exit"};
-         unloadingInterval = 2;
-         loadingDistance = 20;
-         loadingAngle = 60;
-         parachuteClassDefault = "B_Parachute_02_F";
-         parachuteHeightLimitDefault = 5;
-      };
-      class Cargo
-      {
-         canBeTransported = 0;
-      };
-   };
-
-   class TransportItems : TransportItems
-   {
-   };
-   class TransportBackpacks : TransportBackpacks
-   {
-   };
-   class TransportMagazines : TransportMagazines
-   {
-   };
-   class TransportWeapons : TransportWeapons
-   {
-   };
 };
--- addons/PTF_Aircraft/config.cpp
+++ addons/PTF_Aircraft/config.cpp
@@ -1,66 +1,64 @@
 class CfgPatches
 {
-   class PTF_Aircraft
-   {
-      units[] = {
-          "PTF_C130J",
-          "PTF_C130J_Cargo",
-          "PTF_CH53_Ramp",
-          "PTF_CH53_Cargo",
-          "PTF_UH1Y_Unarmed",
-          "PTF_UH1Y_Medevac",
-          "PTF_MV22_Ramp",
-          "PTF_MV22_Cargo",
-          "PTF_MD500",
-          "PTF_Cessna_400",
-          "PTF_MD500_Trainer",
-          "PTF_Cessna_400_Trainer",
-          "PTF_MD500_HQ",
-          "PTF_Cessna_400_HQ",
-          "PTF_AH1Z_GUNFIGHTERS",
-          "PTF_AH1Z_HQ",
-          "PTF_MQ9_SCARFACE",
-          "PTF_MQ9_HQ",
-          "PTF_UH1Y_HQ",
-          "PTF_UH1Y_SCARFACE",
-          "PTF_UH1Y_SCARFAFCE_SL",
-          "PTF_UH1Y_GUNFIGHTERS",
-          "PTF_UH1Y_GUNFIGHTERS_SL",
-          "PTF_RQ4A_HQ",
-          "PTF_C17",
-          "PTF_KC135",
-          "PTF_AV8B",
-		  "PTF_F18C",
-		  "PTF_F18C_CAG",
-		  "PTF_F18D",
-          "PTF_SuperTucano"
-          };
-      weapons[] = {
-          "PTF_weap_mastersafe"};
-      requiredVersion = 1.32;
-      requiredAddons[] = {
-          "PTF_Main",
-          "PTF_Textures",
-          "PTF_Models",
-          "rhsusf_a2port_air2",
-          "CUP_AirVehicles_Core",
-          "CUP_Creatures_Military_USMC",
-          "CUP_AirVehicles_CH47",
-          "rhsusf_ch53",
-          "A3_Air_F_Exp",
-          "A3_Air_F",
-          "CUP_AirVehicles_MV22",
-          "CUP_AirVehicles_MQ9",
-          "ace_interaction",
-          "USAF_C130J",
-          "FIR_AV8B_F",
-          "rhsgref_a29"
-          };
-   };
+	class PTF_Aircraft
+	{
+		units[]=
+		{
+			"PTF_C130J",
+			"PTF_C130J_Cargo",
+			"PTF_CH53_Ramp",
+			"PTF_CH53_Cargo",
+			"PTF_UH1Y_Unarmed",
+			"PTF_UH1Y_Medevac",
+			"PTF_MV22_Ramp",
+			"PTF_MV22_Cargo",
+			"PTF_MD500",
+			"PTF_Cessna_400",
+			"PTF_MD500_Trainer",
+			"PTF_Cessna_400_Trainer",
+			"PTF_MD500_HQ",
+			"PTF_Cessna_400_HQ",
+			"PTF_AH1Z_GUNFIGHTERS",
+			"PTF_AH1Z_HQ",
+			"PTF_MQ9_SCARFACE",
+			"PTF_MQ9_HQ",
+			"PTF_UH1Y_GUNFIGHTERS",
+			"PTF_RQ4A_HQ",
+			"PTF_C17",
+			"PTF_KC135",
+			"PTF_AV8B",
+			"PTF_F18C",
+			"PTF_F18C_CAG",
+			"PTF_F18D",
+			"PTF_SuperTucano"
+		};
+		weapons[]=
+		{
+			"PTF_weap_mastersafe"
+		};
+		requiredVersion=1.3200001;
+		requiredAddons[]=
+		{
+			"PTF_Main",
+			"PTF_Textures",
+			"PTF_Models",
+			"rhsusf_a2port_air2",
+			"CUP_AirVehicles_Core",
+			"CUP_Creatures_Military_USMC",
+			"CUP_AirVehicles_CH47",
+			"rhsusf_ch53",
+			"A3_Air_F_Exp",
+			"A3_Air_F",
+			"CUP_AirVehicles_MV22",
+			"CUP_AirVehicles_MQ9",
+			"ace_interaction",
+			"USAF_C130J",
+			"FIR_AV8B_F",
+			"rhsgref_a29"
+		};
+	};
 };
-
-#include "cfgImport.hpp"
-#include "cfgDefinitions.hpp"
+#include "cfgIMPORT.hpp"
 class cfgVehicles
 {
 #include "cfgAH1Z.hpp"
@@ -72,9 +70,13 @@ class cfgVehicles
 #include "cfgC17.hpp"
 #include "cfgKC135.hpp"
 #include "cfgMV22.hpp"
-#include "cfgUH1Y.hpp"
 #include "cfgAV8B.hpp"
-#include "cfgVanilla.hpp"
+#include "cfgVANILLA.hpp"
 #include "cfgRQ4A.hpp"
-#include "cfgCustom.hpp"
+#include "cfgUH1Y.hpp"
+};	
+class cfgMods
+{
+	author="3S-15 | M.Hawkins";
+	timepacked="1675414171";
 };
