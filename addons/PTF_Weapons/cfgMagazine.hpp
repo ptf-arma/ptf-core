34b58d544e9c3b493ecd2e73b5a09d129d3b7b62
Lost <34311095+FireStone1029@users.noreply.github.com>
2023-07-26 14:56:40 -0400

    Added rhs hellfires
    need to fix two errors before merging

--- addons/PTF_Weapons/cfgMagazine.hpp
+++ addons/PTF_Weapons/cfgMagazine.hpp
@@ -1,11 +1,31 @@
 class cfgMagazines {
+	
 	class PylonRack_4Rnd_ACE_Hellfire_AGM114K;
+
 	class FIR_Hydra_WP_P_7rnd_M;
+
+	class rhs_mag_Hellfire_base{};
+	class rhs_mag_AGM114K : rhs_mag_Hellfire_base{};
+	class rhs_mag_AGM114M : rhs_mag_AGM114K{};
+	class rhs_mag_AGM114M_2 : rhs_mag_AGM114M{};
+	class rhs_mag_AGM114M_4 : rhs_mag_AGM114M_2{};
+
+
 	
 	class PTF_FIR_Hydra_WP_P_7rnd_M : FIR_Hydra_WP_P_7rnd_M {
 		hardpoints[] = {"RHS_HP_FFAR_USMC"};
 		pylonWeapon = "PTF_FIR_RKT_Launcher";
 	};
+
+	class PTF_rhs_mag_AGM114M_2 : rhs_mag_AGM114M_2 {
+		hardpoints[] = {"RHS_HP_FFAR_USMC"};
+		pylonWeapon = "PTF_rhs_weap_AGM114M_base_Launcher";
+	};
+	class PTF_rhs_mag_AGM114M_4 : rhs_mag_AGM114M_4 {
+		hardpoints[] = {"RHS_HP_FFAR_USMC"};
+		pylonWeapon = "PTF_rhs_weap_AGM114M_base_Launcher";
+	};
+
 	class PTF_PylonRack_4Rnd_ACE_Hellfire_AGM114K :PylonRack_4Rnd_ACE_Hellfire_AGM114K {
 		hardpoints[] = {"RHS_HP_FFAR_USMC", "RHS_HP_AIM9_HELI_USMC"};
 		displayName = "4x AGM-114K [Hatchet]";
