b3ecc96e227504596860e120106eb815f98a5e64
Lost <34311095+FireStone1029@users.noreply.github.com>
2024-03-27 21:12:06 -0400

    removed fm

--- addons/PTF_Aircraft/cfgF18.hpp
+++ addons/PTF_Aircraft/cfgF18.hpp
@@ -9,7 +9,6 @@ class PTF_F18C : FIR_F18C
    author = "Paramarine Task Force";
    displayName = "F-18C Hornet [MAG36]";
    hiddenselectionstextures[] = {"\z\PTF\addons\PTF_Textures\aircraft\FA18C\VMFA 542\body_co.paa","\z\PTF\addons\PTF_Textures\aircraft\FA18C\VMFA 542\wing_co.paa","",""};
-   #include "FM\F-18FM.hpp"
    class ACE_Actions : ACE_Actions {
       class ACE_MainActions : ACE_MainActions {
          class F18_Loadouts {
