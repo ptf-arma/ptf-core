bea279c4e218b2beff37ccdd1aa7946808a9d1e4
Lost <Jacob912@live.com>
2024-04-17 20:04:38 -0700

    removed left over from failed idea

--- addons/PTF_Weapons/cfgWeapons.hpp
+++ addons/PTF_Weapons/cfgWeapons.hpp
@@ -7,8 +7,4 @@ class cfgWeapons {
 	class PTF_rhs_weap_AGM114M_base_Launcher: rhs_weap_AGM114M_base_Launcher {
 		magazines[] = {"PTF_rhs_mag_AGM114M_2"};
 	};
-
-	class PTF_rhs_weap_M320 : rhs_weap_M320 {
-		type = 4;
-	};
 };
