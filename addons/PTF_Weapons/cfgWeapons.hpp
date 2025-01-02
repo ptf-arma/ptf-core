0a923ddcecd7ba28f3d0269b0a7a73991884a1db
CatWithAPI <kerrywinters20@gmail.com>
2025-01-01 20:07:20 -0600

    rock from yulkia

--- addons/PTF_Weapons/cfgWeapons.hpp
+++ addons/PTF_Weapons/cfgWeapons.hpp
@@ -7,4 +7,13 @@ class cfgWeapons {
 	class PTF_rhs_weap_AGM114M_base_Launcher: rhs_weap_AGM114M_base_Launcher {
 		magazines[] = {"PTF_rhs_mag_AGM114M_2"};
 	};
+	class Throw: GrenadeLauncher
+	{
+		muzzles[] += {"throwable_Grenade_Muzzle"};
+		class ThrowMuzzle;
+		class throwable_Grenade_Muzzle: ThrowMuzzle
+		{
+			magazines[] = {"ibr_throwable_stone"};
+		};
+	};
 };
