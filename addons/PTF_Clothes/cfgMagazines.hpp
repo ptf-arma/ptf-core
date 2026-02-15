c9886ea6ab92c2a51b2c9fb37a151833f37dd6f3
Goltred <goltred@hotmail.com>
2026-02-14 21:43:34 -0500

    Added MTVR/LCAC cargo modifications. Changed SCAR mag weight to 12 (for 1.2lbs). Updated Whitelist

--- addons/PTF_Clothes/cfgMagazines.hpp
+++ addons/PTF_Clothes/cfgMagazines.hpp
@@ -76,6 +76,7 @@ class PTF_Mag_762x51_MK316_Special_Mix : rhs_mag_20Rnd_SCAR_762x51_mk316_special
    displayName = "20rnd Mk316 Mod 0 (TE3)";
    displayNameShort = "Mk316 (TE3)";
    tracersEvery = 4;
+   mass = 12;
 };
 class PTF_Mag_762x51_M80A1_EPR_Mix : rhs_mag_20Rnd_SCAR_762x51_m80a1_epr
 {
@@ -83,4 +84,101 @@ class PTF_Mag_762x51_M80A1_EPR_Mix : rhs_mag_20Rnd_SCAR_762x51_m80a1_epr
    displayName = "20rnd M80A1 EPR (TE3)";
    displayNameShort = "M80A1 EPR (TE3)";
    tracersEvery = 4;
+   mass = 12;
+};
+
+class PTF_Mag_762x51_20Rnd_SCAR_m118_special: rhs_mag_20Rnd_SCAR_762x51_m118_special
+{
+   author = "Paramarine Task Force";
+   displayName = "20rnd SCAR M118 Ball";
+   displayNameShort = "M118 Ball";
+   mass = 12;
+};
+
+class PTF_Mag_762x51_20Rnd_SCAR_m118_special_bk: rhs_mag_20Rnd_SCAR_762x51_m118_special_bk
+{
+   author = "Paramarine Task Force";
+   displayName = "20rnd SCAR M118 Ball (Black)";
+   displayNameShort = "M118 Ball (Black)";
+   mass = 12;
+}
+
+class PTF_Mag_762x51_20Rnd_SCAR_m61_ap: rhs_mag_20Rnd_SCAR_762x51_m61_ap
+{
+   author = "Paramarine Task Force";
+   displayName = "20rnd SCAR M61 AP";
+   displayNameShort = "M61 AP";
+   mass = 12;
+};
+
+class PTF_Mag_762x51_20Rnd_SCAR_m61_ap_bk: rhs_mag_20Rnd_SCAR_762x51_m61_ap_bk
+{
+   author = "Paramarine Task Force";
+   displayName = "20rnd SCAR M61 AP (Black)";
+   displayNameShort = "M61 AP (Black)";
+   mass = 12;
+};
+
+class PTF_Mag_762x51_20Rnd_SCAR_m62_tracer: rhs_mag_20Rnd_SCAR_762x51_m62_tracer
+{
+   author = "Paramarine Task Force";
+   displayName = "20rnd SCAR M62 Tracer";
+   displayNameShort = "M62 Tracer";
+   mass = 12;
+};
+
+class PTF_Mag_762x51_20Rnd_SCAR_m62_tracer_bk: rhs_mag_20Rnd_SCAR_762x51_m62_tracer_bk
+{
+   author = "Paramarine Task Force";
+   displayName = "20rnd SCAR M62 Tracer (Black)";
+   displayNameShort = "M62 Tracer (Black)";
+   mass = 12;
+};
+
+class PTF_Mag_762x51_20Rnd_SCAR_m80_ball: rhs_mag_20Rnd_SCAR_762x51_m80_ball
+{
+   author = "Paramarine Task Force";
+   displayName = "20rnd SCAR M80";
+   displayNameShort = "M80";
+   mass = 12;
+};
+
+class PTF_Mag_762x51_20Rnd_SCAR_m80_ball_bk: rhs_mag_20Rnd_SCAR_762x51_m80_ball_bk
+{
+   author = "Paramarine Task Force";
+   displayName = "20rnd SCAR M80 (Black)";
+   displayNameShort = "M80 (Black)";
+   mass = 12;
+};
+
+class PTF_Mag_762x51_20Rnd_SCAR_m80a1_epr: rhs_mag_20Rnd_SCAR_762x51_m80a1_epr
+{
+   author = "Paramarine Task Force";
+   displayName = "20rnd SCAR M80A1 EPR";
+   displayNameShort = "M80A1 EPR";
+   mass = 12;
+};
+
+class PTF_Mag_762x51_20Rnd_SCAR_m80a1_epr_bk: rhs_mag_20Rnd_SCAR_762x51_m80a1_epr_bk
+{
+   author = "Paramarine Task Force";
+   displayName = "20rnd SCAR M80A1 EPR (Black)";
+   displayNameShort = "M80A1 EPR (Black)";
+   mass = 12;
+};
+
+class PTF_Mag_762x51_20Rnd_SCAR_mk316_special: rhs_mag_20Rnd_SCAR_762x51_mk316_special
+{
+   author = "Paramarine Task Force";
+   displayName = "20rnd SCAR Mk316 Mod 0";
+   displayNameShort = "Mk316 Mod 0";
+   mass = 12;
+};
+
+class PTF_Mag_762x51_20Rnd_SCAR_mk316_special_bk: rhs_mag_20Rnd_SCAR_762x51_mk316_special_bk
+{
+   author = "Paramarine Task Force";
+   displayName = "20rnd SCAR Mk316 Mod 0 (Black)";
+   displayNameShort = "Mk316 Mod 0 (Black)";
+   mass = 12;
 };
---
+++ addons/PTF_Vehicles/cfgLCAC.hpp
@@ -0,0 +1,12 @@
+//LCAC
+class PTF_LCAC : Peral_LCAC
+{
+	scope = 2;
+	faction = "BLU_F";
+    editorCategory = "PTF";
+    editorSubcategory = "PTF_Armor";
+    ace_cargo_space = 192;
+    ace_cargo_hasCargo = 1;
+    author = "Paramarine Task Force";
+    displayName = "LCAC [PTF]";
+};
