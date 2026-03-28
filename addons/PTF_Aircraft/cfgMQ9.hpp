9525ac07a51205db4e000cc3ff1f00efceb11d60
Goltred <goltred@hotmail.com>
2026-03-28 16:30:31 -0400

    Added new weapon definition for 4rnd 114R. Changed MQ9 pylons definitions

--- addons/PTF_Aircraft/cfgMQ9.hpp
+++ addons/PTF_Aircraft/cfgMQ9.hpp
@@ -28,6 +28,72 @@ class PTF_MQ9 : USAF_MQ9
          };
       };
    };
+   class Components: Components
+   {
+      class TransportPylonsComponent
+      {
+         UIPicture = "USAF_MQ9\Data\UI\mq9_loadout.paa";
+         class presets
+         {
+            class empty
+            {
+               displayName = "Empty";
+               attachment[] = {"","","",""};
+            };
+            class Default
+            {
+               displayName = "Default";
+               attachment[] = {"PTF_PylonRack_4Rnd_AGM114R","USAF_PylonRack_1Rnd_MQ9FuelTank","USAF_PylonRack_1Rnd_MQ9FuelTank","PTF_PylonRack_4Rnd_AGM114R"};
+            };
+            class AGM114P
+            {
+               displayName = "AGM-114P";
+               attachment[] = {"PTF_PylonRack_4Rnd_AGM114P","","","PTF_PylonRack_4Rnd_AGM114P"};
+            };
+            class AGM114R
+            {
+               displayName = "AGM-114R";
+               attachment[] = {"PTF_PylonRack_4Rnd_AGM114R","","","PTF_PylonRack_4Rnd_AGM114R"};
+            };
+         };
+         class pylons
+				{
+					class pylons1
+					{
+						UIposition[] = {0.32,0.1};
+						maxweight = 1000;
+						hardpoints[] = {"B_USAF_MQ9_FUEL","B_BOMB_GBU12","B_BOMB_GBU38","B_BOMB_GBU39","B_BOMB_GBU49","B_BOMB_GBU53","B_BOMB_GBU54","B_BOMB_APKWS7","B_MISSILE_AGM114P_4","B_MISSILE_AGM114P_2","B_MISSILE_AGM114R", "B_MISSILE_AGM114R_4"};
+						attachment = "USAF_PylonRack_4Rnd_AGM114R";
+						priority = 2;
+						turret[] = {0};
+						memPoint = "proxy:\A3\Weapons_F\DynamicLoadout\PylonPod_3x_Missile_AGM_02_F.001";
+					};
+					class pylons2: pylons1
+					{
+						UIposition[] = {0.32,0.2};
+						maxweight = 1000;
+                  hardpoints[] = {"B_USAF_MQ9_FUEL","B_BOMB_GBU12","B_BOMB_GBU38","B_BOMB_GBU39","B_BOMB_GBU49","B_BOMB_GBU53","B_BOMB_GBU54"};
+						attachment = "USAF_PylonRack_1Rnd_MQ9FuelTank";
+						priority = 1;
+						memPoint = "proxy:\A3\Weapons_F\DynamicLoadout\PylonPod_3x_Missile_AGM_02_F.002";
+					};
+					class pylons3: pylons2
+					{
+						UIposition[] = {0.32,0.38};
+						attachment = "USAF_PylonRack_1Rnd_MQ9FuelTank";
+						mirroredMissilePos = 2;
+						memPoint = "proxy:\A3\Weapons_F\DynamicLoadout\PylonPod_3x_Missile_AGM_02_F.003";
+					};
+					class pylons4: pylons1
+					{
+						UIposition[] = {0.32,0.48};
+						attachment = "USAF_PylonRack_4Rnd_AGM114R";
+						mirroredMissilePos = 1;
+						memPoint = "proxy:\A3\Weapons_F\DynamicLoadout\PylonPod_3x_Missile_AGM_02_F.004";
+					};
+				};
+      };
+   };
 };
 class PTF_MQ9_SCARFACE : PTF_MQ9
 {
