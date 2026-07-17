48d77d916b964721861e8eef3ebe0ee1615c30f9
Goltred <goltred@hotmail.com>
2026-07-17 17:13:11 -0400

    Removed extra tfar line as it does nothing

--- addons/PTF_Clothes/helmets/cfgAirHelmets.hpp
+++ addons/PTF_Clothes/helmets/cfgAirHelmets.hpp
@@ -51,13 +51,11 @@ class PTF_Helmet_Air_USA : PTF_Helmet_Air_Black
 };
 class PTF_Helmet_Air_HMDS : PTF_Helmet_Air_Black
 {
-
    tf_encryptionCode = "tf_west_radio_code";
    tf_dialog = "rt1523g_radio_dialog";
    tf_subtype = "digital_lr";
    tf_range = 15000;
    tf_hasLRradio = 1;
-   tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel";
    displayName = "[Air] HMDS (Gen3) [PTF]";
    author = "Paramarine Task Force";
    class ItemInfo : ItemInfo
