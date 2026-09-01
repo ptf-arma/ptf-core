223d62c91ab35408b01ca720f021c9a858097113
Goltred <goltred@hotmail.com>
2026-08-31 21:39:56 -0400

    Cleanup

--- addons/PTF_Scripts/cfgAceActions3rdFlags.hpp
+++ addons/PTF_Scripts/cfgAceActions3rdFlags.hpp
@@ -7,10 +7,10 @@ class PTF_3rd_Flags : PTF_Flag_Raise
         displayName = "Raise 3rd Platoon Flag";
         statement = "0 = [_target, ""3flag.paa""] call PTF_fnc_FlagRaise";
     };
-    class 34_flag_Raise : PTF_Flag_Raise
+    class 31_flag_Raise : PTF_Flag_Raise
     {
-        displayName = "Raise 3-4 Flag";
-        statement = "0 = [_target, ""34flag.paa""] call PTF_fnc_FlagRaise";
+        displayName = "Raise 3-1 Flag";
+        statement = "0 = [_target, ""31flag.paa""] call PTF_fnc_FlagRaise";
     };
     class 32_flag_Raise : PTF_Flag_Raise
     {
@@ -22,10 +22,9 @@ class PTF_3rd_Flags : PTF_Flag_Raise
         displayName = "Raise 3-3 Flag";
         statement = "0 = [_target, ""33flag.paa""] call PTF_fnc_FlagRaise";
     };
-    class 31_flag_Raise : PTF_Flag_Raise
+    class 34_flag_Raise : PTF_Flag_Raise
     {
-        displayName = "Raise 3-1 Flag";
-        statement = "0 = [_target, ""31flag.paa""] call PTF_fnc_FlagRaise";
+        displayName = "Raise 3-4 Flag";
+        statement = "0 = [_target, ""34flag.paa""] call PTF_fnc_FlagRaise";
     };
-
 };
